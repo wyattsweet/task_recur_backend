c7da3505ce95c0b8a72e8d388a471133cb4cad7b
wyattsweet <wyattsweet@gmail.com>
2018-07-03 23:10:17 -0700

    users create route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,10 +6,22 @@ module Api::V1
     end
 
     def create
-
+      user = User.create(new_user_params)
+      if user.valid?
+        jwt = Auth.issue({ user: user.id })
+        render :json => { jwt: jwt }
+      else
+        render status: 400, json: {
+          message: user.errors.full_messages
+        }
+      end
     end
 
     private
+
+    def new_user_params
+      params.permit(:email, :password)
+    end
 
     def get_users
       @user = User.all.select(:id, :email, :name)
